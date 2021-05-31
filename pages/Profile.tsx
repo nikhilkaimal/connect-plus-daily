0b907fc75e6293433f1ab6b95a749dcf5e73c2e6
Nikhil Kaimal <nikhilkjr96@gmail.com>
2021-05-31 13:44:03 +0530

    added name from session in profile

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,14 +1,18 @@
 import { UserIcon } from "@heroicons/react/outline";
+import { session } from "next-auth/client";
+import { useSession } from "next-auth/client";
 
 export default function Profile() {
+  const [session] = useSession();
+
   return (
     <>
       <div className="grid grid-cols-3 text-center space-y-10 p-10">
         <div className="col-span-3">
           <UserIcon className="h-24 mx-auto border border-black" />
         </div>
-        <div>Username</div>
-        <div>Ashikha Aroop</div>
+        <div>Name</div>
+        <div>{session ? session.user.name : "Nikhil"}</div>
         <div className="flex justify-center">
           <button className="btn-simple">Edit</button>
         </div>
